8242b4da3e69e2de1be1cbdaae5ba487700fad21
Thomas Koerner <thomas.koerner@kreuzwerker.de>
2020-06-16 10:17:05 +0200

    add color to the API

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -43,13 +43,13 @@ describe("Pact Verification", () => {
         stateHandlers: {
           "product with ID 10 exists": () => {
             controller.repository.products = new Map([
-                ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")]
             ]);
           },
           "products exist": () => {
             controller.repository.products = new Map([
-              ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-              ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+              ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "red")],
+              ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")]
             ]);
           },
           "no products exist": () => {
